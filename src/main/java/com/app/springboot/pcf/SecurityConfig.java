835a752966739232a8631aa94a419c808e4bf055
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2019-05-21 23:14:03 -0500

    Updated security configurations

---
+++ src/main/java/com/app/springboot/pcf/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.app.springboot.pcf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Anish Panthi
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+}
